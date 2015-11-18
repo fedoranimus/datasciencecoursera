efb29b335199663618f2d4e438e9e558dd0d1c9b
Tim Turner <timdturner@gmail.com>
2015-11-18 18:15:01 -0500

    Wrote cacheSolve to invert a matrix if it's not cached

--- r_programming/week3/hw2/cachematrix.R
+++ r_programming/week3/hw2/cachematrix.R
@@ -22,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse
+        if(!isnull(m)) {
+          message("getting cached data")
+          return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setInverse(m)
+        m
 }
